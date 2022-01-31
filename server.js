fcaa9e4cea9164d61284458e1a615a90927bd651
suma511 <eliansumaia@gmail.com>
2022-01-31 10:04:09 +0200

    edited

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 'use strict' ;
-const express = requier('express') ;
-const cors = requier('cors') ;
+const express = require('express') ;
+const cors = require('cors') ;
 const server = express() ;
 server.use(cors());
 
 
-let moviesdata = requier('./task11/data.json')
-server.get('*',handelNotFound);
+let moviesdata = require('./task11/data.json')
+//server.get('*',handelNotFound);
 server.get('/',handelHomePage);
-server.get('*',handelNotFound);
 server.get('/favorite',handelFavoritePage)
+server.get('*',handelNotFound);
 
-
-function data (title, posterpath , overview )
+function Data (title, posterpath , overview )
 {
     this.title =title ;
     this.posterpath =posterpath ;
@@ -22,27 +21,74 @@ function data (title, posterpath , overview )
 
 function handelHomePage(request,response)
 {
-let obj = new data(moviesdata.title,moviesdata.posterpath,moviesdata.overview) ;
-console.log(data) ;
-return response.status(200).json(data) ;}
+let obj = new Data(moviesdata.title,moviesdata.poster_path,moviesdata.overview) ;
+console.log(obj) ;
+return response.status(200).json(obj) ;}
 
 
 
 function handelFavoritePage(request,response){
-    return response.status(200).("Welcome to Favorite Page") ;
+    return response.status(200).send("Welcome to Favorite Page") ;
 }
 
 
 
-function handelNotFound(request,response)
-response.status(404).send('this page not exist')
+function handelNotFound(request,response){
+response.status(404).send('this page not exist')}
 
 
 
-function handelError(request,response)
-response.status(500).send('server error')
+//function handelError(request,response)
+//response.status(500).send('server error')
+
+server.listen((3000,()=>{
+    console.log("listining to port 3000")
+}))
+
+'use strict' ;
+const express = require('express') ;
+const cors = require('cors') ;
+const server = express() ;
+server.use(cors());
 
 
-server.listen(3000),() =>
-{console.log("hello"); 
+let moviesdata = require('./task11/data.json')
+//server.get('*',handelNotFound);
+server.get('/',handelHomePage);
+server.get('/favorite',handelFavoritePage)
+server.get('*',handelNotFound);
+
+function Data (title, posterpath , overview )
+{
+    this.title =title ;
+    this.posterpath =posterpath ;
+    this.overview =overview ;
+
 }
+
+function handelHomePage(request,response)
+{
+let obj = new Data(moviesdata.title,moviesdata.poster_path,moviesdata.overview) ;
+console.log(obj) ;
+return response.status(200).json(obj) ;}
+
+
+
+function handelFavoritePage(request,response){
+    return response.status(200).send("Welcome to Favorite Page") ;
+}
+
+
+
+function handelNotFound(request,response){
+response.status(404).send('this page not exist')}
+
+
+
+//function handelError(request,response)
+//response.status(500).send('server error')
+
+server.listen((3000,()=>{
+    console.log("listining to port 3000")
+}))
+
