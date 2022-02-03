efe7d3664df122b09386dd0e83e02dfb209de834
suma511 <eliansumaia@gmail.com>
2022-02-03 12:51:26 +0200

    new

--- schema.sql
+++ schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE Movies(
      id SERIAL PRIMARY KEY,
      title VARCHAR(255),
      release_date VARCHAR(255),
-     poster VARCHAR(1000),
-     overview VARCHAR(1000),
-     comment VARCHAR(255)
+     poster_path VARCHAR(255),
+     overview VARCHAR(4000),
+     
 
-)
+);
